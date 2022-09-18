5b8ddb9d9b5fc4172ade633c59bd062b2472996c
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-09-18 07:34:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
+const Booru = require('booru')
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +19,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
+app.get('/', (req, res) => {
+  let posts = await Booru.search('dan', ['yuri'], { limit: 350, random: false })
+  d = posts
+  resu = []
+  d.forEach(ggwp =>{
+  ld = { url: `${ggwp.file_url}` }
+  resu.push(ld)
+  })
+  random =  resu[Math.floor(Math.random() * resu.length)]
+  res.json({url: "https://external-content.duckduckgo.com/iu/?u=" + random.url})
+})
+
+
+
 
 module.exports = app;
